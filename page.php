36bbc285186ded6fd94adc0203838df31b309c7f
hotmotoshopru-cloud <hotmotoshop.ru@gmail.com>
2026-09-17 12:32:21 +0300

    Add AvtoSPA page template

---
+++ page.php
@@ -0,0 +1,1 @@
+<?php get_header(); ?><main class="section"><div class="container"><article><?php while(have_posts()): the_post(); ?><h1><?php the_title(); ?></h1><div><?php the_content(); ?></div><?php endwhile; ?></article></div></main><?php get_footer(); ?>
