0d8fbc7033eb1a128ecdfc9d20ab775f32a8c023
hotmotoshopru-cloud <hotmotoshop.ru@gmail.com>
2026-09-17 14:20:34 +0300

    Fix mobile navigation base URL and phone data

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?> data-whatsapp="<?php echo esc_attr(get_theme_mod('avtospa_whatsapp','')); ?>" data-telegram="<?php echo esc_attr(get_theme_mod('avtospa_telegram','')); ?>">
+<body <?php body_class(); ?> data-whatsapp="<?php echo esc_attr(get_theme_mod('avtospa_whatsapp','')); ?>" data-telegram="<?php echo esc_attr(get_theme_mod('avtospa_telegram','')); ?>" data-home-url="<?php echo esc_url(home_url('/')); ?>" data-phone="<?php echo esc_attr(avtospa_phone_href()); ?>">
 <?php wp_body_open(); ?>
 <header class="site-header">
   <div class="container header-inner">
