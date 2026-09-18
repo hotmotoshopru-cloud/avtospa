cc4f86416d4e23ca16048ed522d39f7a63374286
hotmotoshopru-cloud <hotmotoshop.ru@gmail.com>
2026-09-18 12:03:31 +0300

    Match AvtoSpa reference header structure

--- header.php
+++ header.php
@@ -15,10 +15,6 @@
       <span class="brand-mark" aria-hidden="true"><span>✦</span></span>
       <span class="brand-name">АвтоСпа</span>
     </a>
-    <a class="header-location" href="<?php echo esc_url(home_url('/#contacts')); ?>" aria-label="Адрес АвтоСпа: Москва, Полярный проезд, 18, строение 2">
-      <span class="location-dot" aria-hidden="true">●</span>
-      <span><strong>Москва</strong><br><small>Полярный проезд, 18, стр. 2</small></span>
-    </a>
     <nav class="main-nav" aria-label="Основное меню">
       <?php wp_nav_menu(array('theme_location'=>'primary','container'=>false,'fallback_cb'=>'avtospa_fallback_menu')); ?>
       <a class="nav-external" href="https://xn----7sbbgabsz5cgqkebg.su/" target="_blank" rel="noopener noreferrer"><span aria-hidden="true">🔧</span> Автотех ↗</a>
@@ -27,7 +23,7 @@
       <?php $wa=get_theme_mod('avtospa_whatsapp',''); $tg=get_theme_mod('avtospa_telegram',''); ?>
       <?php if($wa): ?><a class="social-link whatsapp-link" href="<?php echo esc_url($wa); ?>" target="_blank" rel="noopener noreferrer" aria-label="Открыть WhatsApp"><span aria-hidden="true"><svg viewBox="0 0 24 24" focusable="false"><path d="M20.5 3.5A10 10 0 0 0 4.2 15.7L3 21l5.4-1.2A10 10 0 1 0 20.5 3.5Zm-8.4 16a8.3 8.3 0 0 1-4.2-1.1l-.3-.2-3.2.7.7-3.1-.2-.3A8.3 8.3 0 1 1 12.1 19.5Zm4.6-6.2c-.2-.1-1.4-.7-1.6-.8-.2-.1-.4-.1-.6.1l-.8 1c-.1.2-.3.2-.5.1a6.8 6.8 0 0 1-2-1.2 7.6 7.6 0 0 1-1.4-1.8c-.1-.2 0-.4.1-.5l.4-.5.3-.5c.1-.2 0-.4 0-.5l-.7-1.7c-.2-.5-.4-.4-.6-.4h-.5c-.2 0-.5.1-.7.3-.2.2-.9.9-.9 2.2s.9 2.5 1 2.7c.1.2 1.8 2.9 4.5 4 .6.3 1.1.4 1.5.5.6.2 1.2.1 1.7.1.5-.1 1.4-.6 1.6-1.2.2-.6.2-1.1.1-1.2 0-.1-.2-.2-.4-.3Z"/></svg></span><b>WhatsApp</b></a><?php else: ?><span class="social-link whatsapp-link is-placeholder" aria-hidden="true"><span><svg viewBox="0 0 24 24"><path d="M20.5 3.5A10 10 0 0 0 4.2 15.7L3 21l5.4-1.2A10 10 0 1 0 20.5 3.5Z"/></svg></span><b>WhatsApp</b></span><?php endif; ?>
       <?php if($tg): ?><a class="social-link telegram-link" href="<?php echo esc_url($tg); ?>" target="_blank" rel="noopener noreferrer" aria-label="Открыть Telegram"><span aria-hidden="true"><svg viewBox="0 0 24 24" focusable="false"><path d="M21.4 4.2 18.2 19c-.2 1-.8 1.2-1.6.8l-4.5-3.3-2.2 2.1c-.2.2-.4.4-.8.4l.3-4.5 8.1-7.3c.4-.4-.1-.6-.6-.2L7 13.3 2.6 12c-1-.3-1-1 .2-1.4L20 3.9c.8-.3 1.5.2 1.4.3Z"/></svg></span><b>Telegram</b></a><?php else: ?><span class="social-link telegram-link is-placeholder" aria-hidden="true"><span><svg viewBox="0 0 24 24"><path d="M21.4 4.2 18.2 19c-.2 1-.8 1.2-1.6.8l-4.5-3.3-2.2 2.1c-.2.2-.4.4-.8.4l.3-4.5 8.1-7.3c.4-.4-.1-.6-.6-.2L7 13.3 2.6 12c-1-.3-1-1 .2-1.4L20 3.9c.8-.3 1.5.2 1.4.3Z"/></svg></span><b>Telegram</b></span><?php endif; ?>
-      <a class="call-link" href="tel:<?php echo esc_attr(avtospa_phone_href()); ?>"><span class="call-icon" aria-hidden="true">☎</span><span>Позвонить</span></a>
+      <a class="call-link" href="tel:<?php echo esc_attr(avtospa_phone_href()); ?>"><span class="call-icon" aria-hidden="true">☎</span><span class="call-copy"><strong>+7 916 299-98-59</strong><small>Позвонить</small></span></a>
       <button class="mobile-toggle" type="button" aria-expanded="false" aria-controls="mobile-menu" aria-label="Открыть меню"><span class="menu-bars" aria-hidden="true"><i></i><i></i><i></i></span></button>
     </div>
   </div>
