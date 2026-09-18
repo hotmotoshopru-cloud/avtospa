48f12050df1403da4efeb222ecd7814997a16d03
hotmotoshopru-cloud <hotmotoshop.ru@gmail.com>
2026-09-18 12:52:56 +0300

    Улучшить доступность хлебных крошек обычных страниц

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 <main class="section">
  <div class="container">
   <article class="content-page">
-   <nav class="breadcrumbs" aria-label="Хлебные крошки"><a href="<?php echo esc_url(home_url('/')); ?>">АвтоСпа</a><span aria-hidden="true">›</span><span><?php the_title(); ?></span></nav>
+   <nav class="breadcrumbs" aria-label="Хлебные крошки"><a href="<?php echo esc_url(home_url('/')); ?>">АвтоСпа</a><span aria-hidden="true">›</span><span aria-current="page"><?php the_title(); ?></span></nav>
    <?php while(have_posts()): the_post(); ?>
     <h1><?php the_title(); ?></h1>
     <div class="entry-content"><?php the_content(); ?></div>
