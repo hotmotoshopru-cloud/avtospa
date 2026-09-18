a0fb563c12ef3a9eedec6eb934674ae5d1f24487
hotmotoshopru-cloud <hotmotoshop.ru@gmail.com>
2026-09-18 12:57:40 +0300

    Добавить skip link для доступности

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> data-whatsapp="<?php echo esc_attr(get_theme_mod('avtospa_whatsapp','')); ?>" data-telegram="<?php echo esc_attr(get_theme_mod('avtospa_telegram','')); ?>" data-home-url="<?php echo esc_url(home_url('/')); ?>" data-phone="<?php echo esc_attr(avtospa_phone_href()); ?>">
+<a class="skip-link" href="#content">Перейти к содержимому</a>
 <?php wp_body_open(); ?>
 <header class="site-header">
   <div class="container header-inner">
